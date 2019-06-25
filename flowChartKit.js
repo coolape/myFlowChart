e7ec1981fb2c376d11350e06c5f10c09f50d378c
chenbin <chbin12@126.com>
2019-06-25 23:22:24 +0800

    new

---
+++ flowChartKit.js
@@ -0,0 +1,36 @@
+var flowChartKit = {}
+
+flowChartKit.addNode = function()
+{
+
+}
+
+flowChartKit.delNode = function()
+{
+
+}
+
+flowChartKit.addPoint = function()
+{
+
+}
+
+flowChartKit.connect = function()
+{
+
+}
+
+flowChartKit.delConnection = function()
+{
+
+}
+
+flowChartKit.getNodeInfor = function()
+{
+    
+}
+
+flowChartKit.getConnectionInfor = function()
+{
+
+}
