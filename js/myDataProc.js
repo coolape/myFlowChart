e24181f7cc9eac1b05584077c7d7ee3827451696
xchg <xchg@no.com>
2019-07-03 18:32:52 +0800

    m

--- js/myDataProc.js
+++ js/myDataProc.js
@@ -1,44 +1,46 @@
 
-var myDataProc = {
-    [flowChartKit.CallbackTypes.onNewNode]: function (params) {
-        var node = params.node;
-        var nodeData = params.data;
-        var nodeId = node.id;
-        if (nodeData.JPList != null) {
-            //说明是节点组
-            var childNodes = params.children;//列表对象
-            for (i = 0; i < childNodes.length; i++) {
-                // console.log(childNodes[i].id);
-            }
-        }
-        console.log(flowChartKit.CallbackTypes.onNewNode + "==" + nodeId);
-    },
+var myDataProc = {}
+myDataProc.nodes = {}
+myDataProc.constions = {}
 
-    [flowChartKit.CallbackTypes.onClickNode]: function (params) {
-        var node = params.node;
-        var nodeData = params.data;
-        var nodeId = node.id;
-        if (nodeData.JPList != null) {
-            //说明是节点组
+myDataProc[flowChartKit.CallbackTypes.onNewNode] = function (params) {
+    var node = params.node;
+    var nodeData = params.data;
+    var nodeId = node.id;
+    if (nodeData.JPList != null) {
+        //说明是节点组
+        var childNodes = params.children;//列表对象
+        for (i = 0; i < childNodes.length; i++) {
+            // console.log(childNodes[i].id);
         }
+    }
+    console.log(flowChartKit.CallbackTypes.onNewNode + "==" + nodeId);
+}
 
-        console.log(flowChartKit.CallbackTypes.onClickNode + "==" + nodeId);
-    },
-    [flowChartKit.CallbackTypes.onDeleteNode]: function (params) {
-        var deletedId = params.nodeID;
-        console.log(flowChartKit.CallbackTypes.onDeleteNode + "==" + deletedId);
-    },
-    [flowChartKit.CallbackTypes.onClickConnection]: function (connection) {
-        console.log(flowChartKit.CallbackTypes.onClickConnection + "==" + connection.id);
-    },
-    [flowChartKit.CallbackTypes.connection]: function (connection) {
-        console.log(flowChartKit.CallbackTypes.connection + "==" + connection.id);
-    },
-    [flowChartKit.CallbackTypes.connectionDetached]: function (connection) {
-        console.log(flowChartKit.CallbackTypes.connectionDetached + "==" + connection.id);
-    },
-    [flowChartKit.CallbackTypes.connectionMoved]: function (connection) {
-        console.log(flowChartKit.CallbackTypes.connectionMoved + "==" + connection.id);
-    },
+myDataProc[flowChartKit.CallbackTypes.onClickNode] = function (params) {
+    var node = params.node;
+    var nodeData = params.data;
+    var nodeId = node.id;
+    if (nodeData.JPList != null) {
+        //说明是节点组
+    }
+
+    console.log(flowChartKit.CallbackTypes.onClickNode + "==" + nodeId);
+}
+myDataProc[flowChartKit.CallbackTypes.onDeleteNode] = function (params) {
+    var deletedId = params.nodeID;
+    console.log(flowChartKit.CallbackTypes.onDeleteNode + "==" + deletedId);
+}
+myDataProc[flowChartKit.CallbackTypes.onClickConnection] = function (connection) {
+    console.log(flowChartKit.CallbackTypes.onClickConnection + "==" + connection.id);
+}
+myDataProc[flowChartKit.CallbackTypes.connection] = function (connection) {
+    console.log(flowChartKit.CallbackTypes.connection + "==" + connection.id);
+}
+myDataProc[flowChartKit.CallbackTypes.connectionDetached] = function (connection) {
+    console.log(flowChartKit.CallbackTypes.connectionDetached + "==" + connection.id);
+}
+myDataProc[flowChartKit.CallbackTypes.connectionMoved] = function (connection) {
+    console.log(flowChartKit.CallbackTypes.connectionMoved + "==" + connection.id);
 }
 
